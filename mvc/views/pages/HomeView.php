06dcfa658fcac3b668b40efa3cbb3326ac249f86
Emerson Teixeira <etads.sabio@gmail.com>
2024-10-16 12:43:56 -0300

    página home finalizada

--- mvc/views/pages/HomeView.php
+++ mvc/views/pages/HomeView.php
@@ -1,1 +1,25 @@
-<h1>Página Home...</h1>
+<div class="home">
+
+    <div class="centralizar">
+
+        <h1>É Prático</h1>
+        <h2>funções que ajudam você!</h2>
+
+        <section id="projetos">
+            <a href="pdf" class="pdf">
+                <img src="mvc/views/imagens/png_pdf.png" alt="Imagem em png do PDF">
+                <h3>Gerador de PDF</h3>
+                <p>Crie PDF de forma simples e prática</p>
+            </a>
+
+
+            <a href="#" class="carregando">
+                <div class="loading-spinner"> </div>
+                <p>Carregando o próximo...</p>
+            </a>
+
+        </section>
+
+    </div>
+
+</div>
