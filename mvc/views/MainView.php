b0b609302e093170ccfb0543a3a036276d968537
Emerson Teixeira <etads.sabio@gmail.com>
2024-10-17 03:18:08 -0300

    criei uma função para definir titulos de acordo com a página

--- mvc/views/MainView.php
+++ mvc/views/MainView.php
@@ -4,15 +4,32 @@ namespace mvc\views;
 
 class MainView
 {
+
     public static function render($page)
     {
         define('HEADER', 'templates/header.php');
         define('PAGE', 'pages/' . ucfirst($page) . 'View.php');
         define('FOOTER', 'templates/footer.php');
 
+        $titulo = self::gerar_titulo($page);
+
         require HEADER;
         require PAGE;
         require FOOTER;
 
+
+
     }
+
+    public static function gerar_titulo($page)
+    {
+        switch ($page) {
+            case 'pdf':
+                return 'Gerador de PDF';
+            default:
+                return 'Prático';
+        }
+
+    }
+
 }
